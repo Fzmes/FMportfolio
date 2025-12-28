8c8bae106d82668ca289708cc71e6b90f1aecb31
MESKINE Fatima Ezzahra <leeneuha@gmail.com>
2025-12-28 20:17:06 +0100

    Fix gitignore to exclude node_modules and build

--- src/App.js
+++ src/App.js
@@ -4,15 +4,16 @@ import Main from "./containers/Main";
 import { ThemeProvider } from "styled-components";
 import { chosenTheme } from "./theme";
 import { GlobalStyles } from "./global";
+import { HashRouter } from "react-router-dom"; // import HashRouter
 
 function App() {
   return (
     <ThemeProvider theme={chosenTheme}>
       <>
         <GlobalStyles />
-        <div>
+        <HashRouter>
           <Main theme={chosenTheme} />
-        </div>
+        </HashRouter>
       </>
     </ThemeProvider>
   );
